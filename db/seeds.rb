9343e3755c32b93409ae21f2e5bbb3f01933f648
Yurijmi <me@yurijmi.ru>
2014-11-13 05:39:34 +0000

    Fixed an error in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n}@railstutorial.org"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
