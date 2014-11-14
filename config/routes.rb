97e5949484af43c4f3eebec10dcc42f7baba32ae
Yurijmi <me@yurijmi.ru>
2014-11-14 11:51:45 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   
   get 'help'    => 'static_pages#help'
@@ -16,4 +12,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
